79b08d516e72697c809c01390517f89bb5219167
Matthew McKay <matt@mattmckay.org>
2022-03-16 22:23:15 -0500

    Remove extra copy

--- scaler.cpp
+++ scaler.cpp
@@ -17,20 +17,20 @@ void scaler::resized(QSize s) {
 void scaler::newWork() {
     auto workToDo = m_worker->getCopy(m_framebuffer);
     if(workToDo) {
+        m_resizeTime.restart();
 
         QImage img((const uchar*) m_framebuffer.data(), COLUMNS, LINES, COLUMNS * sizeof(uint16_t),QImage::Format_RGB16);
-        m_resizeTime.restart();
-        m_frameBufferImage = img.scaled(m_s.width(),m_s.height(),Qt::KeepAspectRatio);
+
+        if(m_writeBuffer) {
+            m_bufferOne = img.scaled(m_s.width(),m_s.height(),Qt::KeepAspectRatio);
+        } else {
+            m_bufferTwo = img.scaled(m_s.width(),m_s.height(),Qt::KeepAspectRatio);
+        }
         m_resizeTimeLast =
                 ((59.0* m_resizeTimeLast) +
                 (m_resizeTime.nsecsElapsed() / 1000000)) / 60;
         g_ResizeTime = m_resizeTimeLast * g_scaleFactor;
 
-        if(m_writeBuffer) {
-            m_bufferOne = m_frameBufferImage.copy();
-        } else {
-            m_bufferTwo = m_frameBufferImage.copy();
-        }
         {
             QMutexLocker locker(&m_copyMux);
             m_writeBuffer = !m_writeBuffer;
